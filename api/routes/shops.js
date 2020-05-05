ab995957e46ab94fc80dd8b0729d054fb097b03b
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-06 00:13:53 +0900

    やっぱりAUTHORIZATION_KEYにする

--- api/routes/shops.js
+++ api/routes/shops.js
@@ -1,14 +1,12 @@
-const { Router } = require('express')
 const dotenv = require('dotenv')
 const config = dotenv.config().parsed
+const { Router } = require('express')
 const router = Router()
-
-// Mock User
-const users = [
-  { name: 'Alexandre' },
-  { name: 'Pooya' },
-  { name: 'Sébastien' }
-]
+const { google } = require('googleapis')
+const sheet = google.blogger({
+  version: 'v3',
+  auth: 'YOUR API KEY'
+});
 
 /* 店一覧を取得 */
 router.get('/shops', function (req, res, next) {
@@ -27,7 +25,7 @@ router.get('/shops/:id', function (req, res, next) {
 
 /* 店情報追加 */
 router.post('/shops', function (req, res, next) {
-  if (req.params.key != config.API_AUTHORIZATION_KEY){
+  if (req.params.key != config.AUTHORIZATION_KEY){
     res.sendStatus(401)
   }
   res.json(users)
@@ -35,7 +33,7 @@ router.post('/shops', function (req, res, next) {
 
 /* 店情報編集 */
 router.put('/shops/:id', function (req, res, next) {
-  if (req.params.key != config.API_AUTHORIZATION_KEY){
+  if (req.params.key != config.AUTHORIZATION_KEY){
     res.sendStatus(401)
   }
   const id = parseInt(req.params.id)
