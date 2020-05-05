701b0cc1dfc97d3acd2e2f63dbbda5d746adabf3
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-06 00:26:18 +0900

    CORSオッケーにしちゃう

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@ const app = express()
 const shops = require('./routes/shops')
 app.use(shops)
 
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 module.exports = {
   path: '/api',
   handler: app
