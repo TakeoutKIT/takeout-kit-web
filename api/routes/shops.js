b578340f16b5a60b916f479621b0101bd90a40b7
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-06 02:04:50 +0900

    IDからの店舗情報取得実装 あとエラー修正

--- api/routes/shops.js
+++ api/routes/shops.js
@@ -3,24 +3,54 @@ const config = dotenv.config().parsed
 const { Router } = require('express')
 const router = Router()
 const { google } = require('googleapis')
-const sheet = google.blogger({
-  version: 'v3',
-  auth: 'YOUR API KEY'
-});
+const sheets = google.sheets({
+  version: 'v4',
+  auth: config.GSHEET_API_KEY
+})
 
 /* 店一覧を取得 */
-router.get('/shops', function (req, res, next) {
-  res.json(users)
+router.get('/shops', async function (req, res, next) {
+  /*
+  const data = await sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: config.GSHEET_ID,
+      range: 'A' + (id+1) + ':K' +  (id+1),
+    }
+  )
+  if (!data) {
+    throw new Error('Invalid google response');
+  }
+  */
+ const page = req.query.page ? parseInt(req.query.page) : 1
+ res.json()
 })
 
 /* 店情報取得 */
-router.get('/shops/:id', function (req, res, next) {
+router.get('/shops/:id', async function (req, res, next) {
   const id = parseInt(req.params.id)
-  if (id >= 0 && id < users.length) {
-    res.json(users[id])
-  } else {
-    res.sendStatus(404)
+  const data = await sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: config.GSHEET_ID,
+      range: 'A' + (id+1) + ':K' +  (id+1),
+    }
+  )
+  if (!data) {
+    throw new Error('Invalid google response');
   }
+  const resp = data.data.values.map(d => ({
+    id: d[0],
+    name: d[1],
+    tag: d[2].split(','),
+    takeout: ( d[3] == 'TRUE' ),
+    firstDelivery: ( d[4] == 'TRUE' ),
+    thirdDelivery: ( d[5] == 'TRUE' ),
+    orderUrl: d[6],
+    siteurl: d[7],
+    imageUrl: d[8],
+    mapUrl: d[9],
+    extraInfo: d[10]
+  }))[0]
+  res.json(resp)
 })
 
 /* 店情報追加 */
