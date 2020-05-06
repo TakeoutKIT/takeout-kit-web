5f9743a08c58762f9bc2b5fe4328f8f91032f79b
zukky <34536327+SuzukiDaishi@users.noreply.github.com>
2020-05-07 00:43:23 +0900

    shopのページを作った

---
+++ pages/shops/_id.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ shopData }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+    async asyncData(context) {
+        //const id = context.params.id
+        //const shopData = (await axiosBase.get(`/api/shops/${id}`)).data
+        return { shopData: 'TODO:ここから' }
+    }
+}
+</script>
+
+<style>
+
+</style>
