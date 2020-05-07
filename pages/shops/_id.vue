95c455473eb17050a18a5516fd957a9e3129f8e1
zukky <34536327+SuzukiDaishi@users.noreply.github.com>
2020-05-08 00:14:22 +0900

    マップを表示

--- pages/shops/_id.vue
+++ pages/shops/_id.vue
@@ -1,10 +1,28 @@
 <template>
     <div>
+        <component-title :title="shopData.name" />
+        <v-row align="center" class="pl-5 pr-5" justify="center">
+            <!-- <v-img
+                src="https://bad.src/not/valid"
+                lazy-src="https://picsum.photos/id/11/100/60"
+                aspect-ratio="1"
+                class="grey lighten-2"
+                max-width="800"
+                max-height="400"
+            >
+            </v-img> -->
+            <iframe 
+                :class="$style.map_embed" 
+                :src="`https://maps.google.co.jp/maps?t=m&output=embed&q=${shopData.name}`" 
+                scrolling="no"
+            ></iframe>
+        </v-row>
         {{ shopData }}
     </div>
 </template>
 
 <script>
+import ComponentTitle from '~/components/molecules/ComponentTitle.vue'
 
 export default {
     validate(context) {
@@ -14,6 +32,16 @@ export default {
         const id = context.params.id
         const shopData = await context.$axios.get(`shops/${id}`)
         return { shopData: shopData.data }
+    },
+    components: {
+        ComponentTitle
     }
 }
 </script>
+
+<style lang="scss" module>
+.map_embed {
+    width: 100%;
+    height: 60vh;
+}
+</style>
